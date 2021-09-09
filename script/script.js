e5d1959a9c94a08974e56b17512d877a294f78d8
BadNerd <86760043+BadNerd@users.noreply.github.com>
2021-09-09 19:50:01 +0700

    fixing ui and animation

--- script/script.js
+++ script/script.js
@@ -2,35 +2,71 @@ const image = document.querySelector('.image img');
 const textbox = document.querySelector('.text');
 const text = textbox.children[0];
 
+let processing = true;
 let scene = 1;
+let active = true;
+loadText('Gently weaving it\'s way through trees and bushes, lightly caressing every marsh, puddle and stream it comes across, the wind calls. It enchants it\'s prey, luring them deeper within the thick folds of Anwir forest.')
+
+
 document.addEventListener('keyup',function(e){
-  if(e.key == 'Enter')
+  if(e.key == 'Enter' && processing)
   {
-    scene++
+    processing = false;
+    console.log(processing);
+
+  }else if(e.key == 'Enter' && !processing)
+  {
+    scene++;
+    processing = true;
   switch(scene){
     case 2 :
-      text.innerHTML = 'The words whispered differ depending on the person, but they all serve the same purpose, to decieve it\'s listener.Some say they hear the voices of young children,others a deceased loved one, all beckoning them to step closer, to enter.These are of course the accounts of those wise enough to not have adhered to the calls of the wind.Even so, it seems one is damned in one way or another the moment they lay foot on the forest floor.';
+      loadText('The words whispered differ depending on the person, but they all serve the same purpose, to decieve it\'s listener.Some say they hear the voices of young children,others a deceased loved one, all beckoning them to step closer, to enter.These are of course the accounts of those wise enough to not have adhered to the calls of the wind.Even so, it seems one is damned in one way or another the moment they lay foot on the forest floor.')
       break;
     case 3 :
-      text.innerHTML = 'So the new rumor says anyway.'
+      loadText('So the new rumor says anyway.')
       break;
     case 4 :
       image.src = 'img/village.jpg'
-      text.innerHTML = 'I live in a small village called Manokawah, about 10 miles or so from Akowah, where the Anwir forest is supposedly situated.'
+      loadText('I live in a small village called Manokawah, about 10 miles or so from Akowah, where the Anwir forest is supposedly situated.')
       break;
     case 5 : 
-      text.innerHTML = 'I had always been a sceptic of all things supernatural, and thus, I got into the business of debunking them.So when I\'d caught on to this new rumor spreading around town, it undoubtedly sparked an interest.'
+      loadText('I had always been a sceptic of all things supernatural, and thus, I got into the business of debunking them.So when I\'d caught on to this new rumor spreading around town, it undoubtedly sparked an interest.')
       break;
     case 6 :
       image.src = 'img/picking-up.jpg'
-      text.innerHTML = 'I called up a couple buddies at 4 AM the next morning on July 7th of 1980.I set out with my bestfriends, Tim and Jack'
+      loadText('I called up a couple buddies at 4 AM the next morning on July 7th of 1980.I set out with my bestfriends, Tim and Jack')
       break;
     case 7 :
-      text.innerHTML = 'We arrived at Akowah a few days later, excited and just as determined to find answers as we were when we first set out.We met up with a friend of mine there, who agreed to take us there, and way too soon it seemed, we found ourselves facing the infamous forest.'
+      loadText('We arrived at Akowah a few days later, excited and just as determined to find answers as we were when we first set out.We met up with a friend of mine there, who agreed to take us there, and way too soon it seemed, we found ourselves facing the infamous forest.')
+      break;
     case 8 :
       image.src = 'img/me-and-the-boys.jpg'
-      text.innerHTML = 'The air was thick and the atmosphere tense, but we had come too far to weasel out now! So with heart\'s pounding, chests puffed and  heads held high, we pressed on and entered Awir.'
+      loadText('The air was thick and the atmosphere tense, but we had come too far to weasel out now! So with heart\'s pounding, chests puffed and  heads held high, we pressed on and entered Awir.')
+      break;
   }
+  console.log(processing);
   }
   
 })
+
+function loadText(txt){
+  text.innerHTML = '';
+  let max = txt.length;
+  let i = 0;
+  let loadChar = null;
+  loadChar = setInterval(function(){
+
+    text.innerHTML += txt[i];
+    i++;
+  
+    if(i == max){
+      clearInterval(loadChar);
+    }
+    if(!processing){
+      clearInterval(loadChar);
+      text.innerHTML = txt;
+    }
+  },50)
+}
+
+
